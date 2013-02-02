56fa19b212346d30adb3b42928046d6c918a85a7
Ashutosh Tiwari <ashutosh.tiwari@vinsol.com>
2013-02-02 17:54:07 +0530

    Intial commit for 'git-exercise'

---
+++ git-exercise.rb
@@ -0,0 +1,5 @@
+1.Create a repository
+*On github i have created a account and create a repository 'exercises' on using 'create new repo' link, to commit the exercises of http://vtapp.vinsol.com.
+*On my system i made a dir in/opt named 'exercises' and then intialize with empty git by using command 'git init'
+
+2.Make some changes -- add some files, change content of some files
