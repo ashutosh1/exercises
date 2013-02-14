30c645a5200646be2486ae5162df7a9386a33537
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-02-14 15:15:39 +0530

    Recommiting the memory game exercise of javascript with more modular approach

--- javascript-exercise/memory-game.js
+++ javascript-exercise/memory-game.js
@@ -1,66 +1,80 @@
 //variable to track time ,count and total click count.
-var timer = 0;
 var count = 0;
 var clickCount = 0;
 var totalMatchedElement = 0; 
 var firstElement = '';
 //start the game and it will fetch all the image and load it in columns and then hide those. 
 function startGame(){
-  var images = getImagesName();
+  loadImagesInColumns(getImagesName());
+  //start the timer
+  startTheTimer();
+  //show stop button after games start.
+  document.getElementById('button2').style.display = 'inline';
+}
+//load images in each column.
+function loadImagesInColumns(images) {
   var j = 0;
   var columns = document.getElementsByTagName('td');
   for(i=0;i<columns.length;i++){
     columns[i].innerHTML = "<img  src=" + images[j] + " alt= The Image " + "width=45 height=45 >" ;
     columns[i].getElementsByTagName('img')[0].style.display = 'none';
     (j == 17) ? j=0 : j++;
-    columns[i].onclick=function(){matchCard(this)};
+    columns[i].onclick=function(){trackElementsAndCounts(this)};
   }
-  //start the timer
-  timer = setInterval(function(){gameTimer()},1000);
-  document.getElementById('button2').style.display = 'inline';
 }
 //timer to track the time to finish the game.
-function gameTimer() {
-  var d=new Date();
-  var t=d.toLocaleTimeString();
-  //show time to user.
-  document.getElementById("timer").innerHTML = t;
+function startTheTimer() {
+  var time = 0;
+  timedCount(time);
+}
+//counting the time 
+function timedCount(time){
+  document.getElementById("timer").innerHTML = time + ' sec';
+  time += 1;
+  t = setTimeout("timedCount(" + time + ")",1000);
+}
+//function to stop the timer 
+function stopTheTimer() {
+  clearTimeout(t);
 }
 //the main logic for comparing the images and then showing or hide the images and counting total click count.
-function matchCard(element){
+function trackElementsAndCounts(element){
   element.getElementsByTagName('img')[0].style.display = 'block';
   count += 1;
   clickCount += 1;
-  //show total click count ot user.
+  //show total click count to user.
   document.getElementById('total_clicks').innerHTML = clickCount;
-  var currentElement = element.getElementsByTagName('img')[0];
   //keep track for last clicked image to match the both.
   if(count == 1){
     firstElement = element.getElementsByTagName('img')[0];
   }
   else if(count == 2){
-    //write the logic to match the element if they match do not hide them otherwise hide them. and set count to 0 again.
-    if(currentElement.src == firstElement.src){
-      totalMatchedElement += 2; 
-      if(totalMatchedElement == 36){
-        stopGame();
-      }
-    }
-    else{
-      //hide after one second if images are not matched. 
-      setTimeout(function(){hideElements(firstElement,currentElement)},1000);
-    }
-   count = 0;
+    comparePic(element);
+    count = 0;
   }
 }
-//hide the pics if not matched.
+//write the logic to match the element if they match do not hide them otherwise hide them. and set count to 0 again.
+function comparePic(element) {
+  var currentElement = element.getElementsByTagName('img')[0];
+  if(currentElement.src == firstElement.src){
+    totalMatchedElement += 2; 
+    if(totalMatchedElement == 36){
+      stopGame();
+    }
+  }
+  else{
+    //hide after one second if images are not matched. 
+    setTimeout(function(){hideElements(firstElement,currentElement)},500);
+  }
+}
+//hide the pics if not matched after one second .
 function hideElements(firstElement,currentElement){
   firstElement.style.display = 'none';
   currentElement.style.display = 'none';   
 }
 //stop the game 
 function stopGame() {
-  clearInterval(timer);
+  stopTheTimer();
   document.getElementById('button1').disabled = true;
   document.getElementById('button2').disabled = true;
   document.getElementById('button3').style.display = 'inline';
@@ -70,10 +84,11 @@ function stopGame() {
   }
   else{
     alert("You have not finished the game!!\nIf you want to start a fresh game click on 'Play Again'");
-    var columns = document.getElementsByTagName('td');
-    for(i=0;i<columns.length;i++){
-      columns[i].onclick = '';
-    }
+  }
+  var columns = document.getElementsByTagName('td');
+  //disable onclick function after game stopped.
+  for(i=0;i<columns.length;i++){
+    columns[i].onclick = '';
   }
 }
 //after clicking on play again show the new window.
@@ -81,7 +96,7 @@ function playAgain(){
   var url = window.location.href; 
   window.open (url, "_self","menubar=no,resizable=no,dependent=no,status=no,scrollbars=no"); 
 }
-//onload of body get image in all table but do not show it.
+//get image name to load in columns.
 function getImagesName(){
   var imagArr = [];
   for(i=0; i<=17; i++){
