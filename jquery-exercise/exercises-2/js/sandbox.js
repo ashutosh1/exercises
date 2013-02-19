567525ec25157af6c342653ede8631dc636a9097
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-02-19 16:01:42 +0530

    Changed Js according to comment

--- jquery-exercise/exercises-2/js/sandbox.js
+++ jquery-exercise/exercises-2/js/sandbox.js
@@ -5,14 +5,12 @@ $(document).ready(function (){
     console.log(index + ': ' + $(this).attr('alt'));
   });
   //Select the search input text box, then traverse up to the form and add a class to the form.
-  console.log( $( "input[name='q']" ).parent().addClass ('myclass'));
+  console.log( $( "input[name='q']" ).closest('form').addClass ('myclass'));
   /*Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item... inside #myList the li has 'current bar' class.*/
-  console.log($( 'li[class="current bar"]').removeClass('current bar').next().addClass('current'));
+  console.log($( 'li[class="current bar"]').removeClass('current').next().addClass('current'));
   //Select the select element inside #specials; traverse your way to the submit button.
   console.log( $ ( '#specials select').closest('form').find('input[type = "submit"][value = "Go"]'));
-  //Select the first list item in the #slideshow element; add the class "current" to it
-  console.log($ ('#slideshow li:nth-child(1)').addClass('current'));
-  //add a class of "disabled" to its sibling elements.
-  console.log($ ('#slideshow li:nth-child(1)').siblings().addClass('disabled'));
+  //Select the first list item in the #slideshow element; add the class "current" to it and a class 'disabled' to its siblings.
+  console.log($('#slideshow li:nth-child(1)').addClass('current').siblings().addClass('disabled'));
 });
 
