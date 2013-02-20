66c10b1b833d518c1e233457f9a50dba5ea5ece6
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-02-20 18:05:17 +0530

    Last exercise of jquery

---
+++ jquery-exercise/exercises-11/stack-example.js
@@ -0,0 +1,20 @@
+$('document').ready(function(){
+  $stack = $("#stack");
+  var count = 0;
+  $('button').click(function () {
+    count++;
+    $item = $('<div>' + count + '</div>');
+    $item.addClass("item");
+    $stack.prepend($item);
+  });
+  $('div.item').live("click", function (){
+    if ($(this).text() == $('div.item:first').text()) {
+      $(this).remove();
+      count--;
+    }
+    else {
+      $(this).attr('style', 'background-color:yellow');
+    }
+  });
+});
+
