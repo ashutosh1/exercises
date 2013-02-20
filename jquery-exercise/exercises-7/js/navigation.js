938df6430f262f3f85754f072a981a1259d0e471
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-02-20 15:23:38 +0530

    Changed as per comment for seventh exercise

--- jquery-exercise/exercises-7/js/navigation.js
+++ jquery-exercise/exercises-7/js/navigation.js
@@ -1,8 +1,12 @@
 //Create Drop down menus.
 //hover effect on ul of li under ul of id 'nav'
 $(document).ready(function(){
-  $('#nav li').hover(function () { 
-    $(this).find ('ul').css("display", "block");} ,
-    function(){ $(this).find ('ul').css ("display", "none");}
+  $('#nav > li').hover(function () { 
+      $(this).addClass('selected').siblings().removeClass('selected');
+      $(this).find('ul').addClass('present');
+    } ,
+    function(){
+      $(this).find ('ul').removeClass("present");
+    }
   );
 });
