034e6998fdd56f423566a4c870b50b82e9816448
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-02-21 13:27:57 +0530

    changed js

--- jquery-exercise/exercises-8/js/slideshow.js
+++ jquery-exercise/exercises-8/js/slideshow.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
-  //Move the #slideshow element to the top of the body.
   $('body').prepend($('#slideshow'));
-  /*Write code to cycle through the list items inside the element; fade one in, display it for a few seconds, then fade it out and fade in the next one.*/
   $('#slideshow li').hide();
   $newDiv = $('<div><div/>').insertAfter($('#slideshow')).attr("class","newdiv");
-  var i = 1;
+  var i = 0;
   var length = $ ("#slideshow li").size();
-  $('#slideshow li:first').fadeIn();
-  showCountAndCurrentImage(length,i);
-  var mainFunction = setInterval(function(){showAndHide()},5000 );
+  showCountAndCurrentImage(length,i)
   function showAndHide(){
-    $("#slideshow li:nth-child(" + i + ")").fadeOut(1000);
-    if(i == length){i = 1;}
-    else{ i += 1;}
-    $("#slideshow li:nth-child(" + i + ")").delay(1000).fadeIn(2000);
-    showCountAndCurrentImage(length,i);
+     $("#slideshow li:eq(" + i + ")").fadeIn(1000,function() {showCountAndCurrentImage(length,i)}).delay(1000).fadeOut(2000,function() {rotateImage()});
   }
+  function rotateImage(){
+    if (i == (length-1)){i = 0;}
+    else{i++;}
+    showAndHide();
+  }
+  showAndHide();
   //to show total images count and currently viewing pic.
   function showCountAndCurrentImage(len,child){
-    $newDiv.text('Total images are ' + len + ' currently viewing '+ $("#slideshow li:nth-child(" + child + ")").find('h2').text());
+    $newDiv.text('Total images are ' + len + ' currently viewing '+ $("#slideshow li:eq(" + child + ")").find('h2').text());
   }
 });
+
