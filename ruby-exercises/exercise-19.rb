bdffddc6b65125204f0848a04b01d2199c6b9083
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-14 16:05:55 +0530

    recommit

--- ruby-exercises/exercise-19.rb
+++ ruby-exercises/exercise-19.rb
@@ -1,14 +1,14 @@
 puts "Enter Principle Amount"
-p = gets.chomp
+principle = gets.chomp
 puts "Enter The Duration"
-t = gets.chomp
+time = gets.chomp
 class Interest
-  attr_accessor :t, :p
+  attr_accessor :time, :principle
   def difference
-    r = 10
-  	amount_simple =  t*p*r/100
-  	amount_compound = p * ((1 + (r/100.0))**t)
-  	puts "diference is #{amount_compound - amount_simple}"
+    rate = 10.0
+  	amount_simple =  time * principle * rate / 100
+  	amount_compound = principle * ((1 + (rate/100)) ** time)
+  	puts "diference is #{(amount_compound - amount_simple).round(2)}"
   end
 end
-obj = Interest.new.tap{|a| a.p, a.t = p.to_i, t.to_i}.difference
+Interest.new.tap{|obj| obj.principle, obj.time = principle.to_i, time.to_i}.difference
