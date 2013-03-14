de281a6260918fb81bf139668ba587333e10fae6
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-14 14:08:49 +0530

    recommit

--- ruby-exercises/exercise-10.rb
+++ ruby-exercises/exercise-10.rb
@@ -1,36 +1,19 @@
-#Using yield and !n/(!k * (!n-k)) method to evaluate each value of pascal tringal
-#Use yield true to avoid puts and in that case compare the value in block
 class Pascal 
-  def pas(m)
-    for i in 0..m
+  def pascal(m)
+    m.times do |i|
       k = 0 
       while (k < i+1)
         yield(fact(i)/(fact(k)*fact(i-k)))
         k += 1
       end
-     yield true #we can use puts also ,in that case no need to check the value of r,we can just print the result
+     yield true 
     end
   end
 
   def fact(n)
-    exit(0) if n < 0
-    return 1 if n <=1
-    (2..n).inject {|s,i| s *= i}
+    return "Please enter number greater than zero" if n < 0
+    n <= 1 ? 1 : n * fact(n-1)  
   end
 end
-obj = Pascal.new 
 #using yield ones for each row and then calling newline before next row starts
-obj.pas(6) {|r| print r== true ? "\n" : "#{r} "}
-
-
-
-#def pascal(n)
-# return if n < 1 
-# arr = [[1]]
-# (n - 1).times do |i|
-#   arr << arr[i].inject([1]) {|res, elem| (arr[i].length == res.length) ? res << 1 : res << elem + arr[i][res.length]}
-#  end
-#  arr.each{|i| puts i.join("  ")}
-#end
-#pascal(6)
-
+Pascal.new.pascal(6) {|r| print r== true ? "\n" : "#{r} "}
