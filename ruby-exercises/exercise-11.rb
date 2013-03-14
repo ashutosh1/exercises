8ee400a4d577a983bd1d59aa7a665d3edfeba566
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-14 15:35:00 +0530

    Recommit

--- ruby-exercises/exercise-11.rb
+++ ruby-exercises/exercise-11.rb
@@ -1,18 +1,17 @@
-class Array
-  def make_hash
-    hash = Hash.new(Array.new)
-    value = self.inject({}) do|res,elm|
-      res.has_key?(elm.to_s.length.to_i) ? res[elm.to_s.length.to_i].push(elm) : res[elm.to_s.length.to_i] = [elm]
-      res 
-    end  
-    value.inject(0) do|res,(k,v)|
-      key = k.odd? ? "odd" : "even" 
-      hash[key] += [v]
-    end
-    puts hash
+def make_hash(array)
+  values = Hash.new(Array.new)
+  result = Hash.new(Array.new)
+  for elm in array
+    values[elm.to_s.length] += [elm.to_s]
   end
-end  
-['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ','xyxyxy','vinsol'].make_hash
+  values.inject(0) do|res,(k,v)|
+    key = k.odd? ? "odd" : "even" 
+    result[key] += [v]
+  end
+  return result
+end
+
+puts make_hash(['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ','xyxyxy','vinsol'])
 
 
  
