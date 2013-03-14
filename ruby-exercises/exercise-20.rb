a3ca02380d530ed90124e3868bdd9b823aff830f
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-14 14:32:42 +0530

    recommit

--- ruby-exercises/exercise-20.rb
+++ ruby-exercises/exercise-20.rb
@@ -2,6 +2,8 @@ def factorial(n)
   begin
     raise "Negative number entered" if n < 0
     n == 0 ? 1 : n * factorial(n-1)
+  rescue Exception => e
+    puts e.message
   end
 end
 puts factorial(5)
