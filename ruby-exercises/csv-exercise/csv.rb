53bfdfdb48ded91d95d59cc096c4dfde092b31cd
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-14 13:37:26 +0530

    Recommit

--- ruby-exercises/csv-exercise/csv.rb
+++ ruby-exercises/csv-exercise/csv.rb
@@ -1,7 +1,7 @@
 require 'csv'
 hash = Hash.new(Array.new)
 CSV.open('a.csv','w') do |csv|
-  CSV.foreach("/opt/exercises/ruby-exercises/csv-exercise/info.csv") do |line|
+  CSV.foreach("exercises/ruby-exercises/csv-exercise/info.csv") do |line|
     hash[line[2]] += [line[0] + '(Empid: ' + line[1] + ')']
   end
   hash.each do|key,val|
