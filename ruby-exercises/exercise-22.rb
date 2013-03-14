9e073165362705f41c3efe30ce9e28b6310130a2
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-14 16:27:56 +0530

    commit

--- ruby-exercises/exercise-22.rb
+++ ruby-exercises/exercise-22.rb
@@ -1,8 +1,9 @@
-class Array 
+class Array
   def reverse_iterate
-    yield self.reverse.join(' ')
+    for i in (1..self.length)
+      yield self[-i] 
+    end
   end
 end
 [2,4,6,8].reverse_iterate{|i| puts i}
-
 
