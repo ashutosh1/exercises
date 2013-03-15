cc0a983421406b26a78baa04f6f2fb31e5e5f3a0
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-15 12:51:51 +0530

    recommit

--- ruby-exercises/exercise-10.rb
+++ ruby-exercises/exercise-10.rb
@@ -2,8 +2,8 @@ class Pascal
   def pascal(m)
     m.times do |i|
       k = 0 
-      while (k < i+1)
-        yield(fact(i) / (fact(k) * fact(i-k)))
+      while (k < i + 1)
+        yield(fact(i) / (fact(k) * fact(i - k)))
         k += 1
       end
       yield true 
@@ -12,8 +12,8 @@ class Pascal
 
   def fact(n)
     return "Please enter number greater than zero" if n < 0
-    n <= 1 ? 1 : n * fact(n-1)  
+    n <= 1 ? 1 : n * fact(n - 1)  
   end
 end
 #using yield ones for each row and then calling newline before next row starts
-Pascal.new.pascal(6) {|r| print r== true ? "\n" : "#{r} "}
+Pascal.new.pascal(6) {|r| print r == true ? "\n" : "#{r} "}
