6b80e646329ed86f668358cdc1d04d128fad0012
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-16 17:58:47 +0530

    commit

--- ruby-exercises/exercise-14.rb
+++ ruby-exercises/exercise-14.rb
@@ -1,16 +1,8 @@
 def count_char (str)
-  upper, digit ,lower, special = 0,0,0,0
-  str.split('').each do |s|
-	  if (('A'..'Z').include?(s))
-	    upper += 1
-	  elsif (('a'..'z').include?(s))
-		  lower += 1
-    elsif (('0'..'9').include?(s))
-	    digit += 1
-	  else 
-		  special += 1
-	  end    
-  end
+  upper = str.scan(/[A-Z]/).count
+  lower = str.scan(/[a-z]/).count
+  digit = str.scan(/[0-9]/).count
+  special = str.length - (upper + lower + digit)
   ['upper','lower','digit','special'].each{|i| puts "Number of #{i} #{i == 'digit' ? '' : 'case'} charchters: #{eval(i)}"}
 end
 count_char("SAM123@vinsol#$6547A%Sd78*&^ASbfg1")
