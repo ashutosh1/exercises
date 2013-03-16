a4bb42282414ba99e42b3dd37a392cb8ed93b433
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-16 15:31:07 +0530

    recommit

--- ruby-exercises/exercise-19.rb
+++ ruby-exercises/exercise-19.rb
@@ -6,7 +6,7 @@ class Interest
   attr_accessor :time, :principle
   def difference
     rate = 10.0
-  	amount_simple =  time * principle * rate / 100
+  	amount_simple =  principle + (time * principle * rate / 100)
   	amount_compound = principle * ((1 + (rate/100)) ** time)
   	puts "diference is #{(amount_compound - amount_simple).round(2)}"
   end
