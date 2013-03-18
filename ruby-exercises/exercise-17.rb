3ebb4df206db68676eb4e1c861877cf9a0e66441
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-18 15:01:05 +0530

    commit

--- ruby-exercises/exercise-17.rb
+++ ruby-exercises/exercise-17.rb
@@ -1,13 +1,14 @@
 require 'time'
-def sumtime (t1, t2)
-  if t1.match(/^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/) && t2.match(/^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/)
-    first_time = Time.parse(t1)
-    second_time = Time.parse(t2)
+def sumtime (first_time, second_time)
+  pattren = /^([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/
+  if first_time.match(pattren) && second_time.match(pattren)
+    first_time = Time.parse(first_time)
+    second_time = Time.parse(second_time)
     sum = first_time + (second_time.sec + (second_time.min * 60) + (second_time.hour * 60 * 60))
-    puts "#{sum.day - first_time.day} day & #{sum.to_s.slice(/[0-9][0-9]?:[0-9][0-9]?:[0-9][0-9]?/)}"
+    "#{sum.day - first_time.day} day & #{sum.strftime('%H:%M:%S')}"
   else
-    puts "Please enter the time in 'HH:MM:SS' format"
+    "Please enter the time in 'HH:MM:SS' format"
   end    
 end
-sumtime('22:12:34', '12:34:23')
+puts sumtime('22:12:34', '12:34:23')
 
