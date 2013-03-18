9296285aa9bff18208ed4f1aa58f0ba64fd10f91
Ashutosh Tiwari <ashutosh@harsh.(none)>
2013-03-18 14:00:33 +0530

    commit

--- ruby-exercises/exercise-15.rb
+++ ruby-exercises/exercise-15.rb
@@ -1,8 +1,10 @@
+puts "Enter the number till you want to list prime numbers."
+num = gets.chomp.to_i
 require 'prime'
-def prime_number(num)
+def print_prime_numbers(num)
   primes = [2]
   3.step(num, 1){|i| primes.push(i) if i.prime?}
   #(3..num).step(1){|i| primes.push(i) if i.prime?}
   primes.join(' ')
 end
-puts prime_number(100)
+puts print_prime_numbers(num)
